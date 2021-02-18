272e792cc3ed60e69088d2ea494027850f9896e6
Timothy <timbo.koo@gmail.com>
2021-02-17 23:18:08 -0800

    First implementation of uniform cost search

--- main.cpp
+++ main.cpp
@@ -1,21 +1,71 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <chrono>
 #include <sstream>
 
 using namespace std;
+using namespace std::chrono; 
 
 class node {
-    vector<vector<int>> boardState;
-    int fCost = 0;
-    int depth = 0;
+    public:
+        vector<vector<int>> boardState;
+        int fCost;
+        int depth;
+        int zeroRow;
+        int zeroColumn;
+        node();
+        node(vector<vector<int>>);
+        node(vector<vector<int>>, int, int);
 };
+
+node::node() {
+    fCost = 0;
+    depth = 0;
+    zeroRow = 0;
+    zeroColumn = 0;
+}
+
+node::node(vector<vector<int>> currBoard) {
+    boardState = currBoard;
+    for(int i = 0; i < boardState.size(); i++) {
+        for(int j = 0; j < boardState.size(); j++) {
+            if(boardState[i][j] == 0) {
+                zeroRow = i;
+                zeroColumn = j;
+                break;
+            }
+        }
+    }
+    fCost = 0;
+    depth = 0;
+}
+
+node::node(vector<vector<int>> currBoard, int fCost, int depth) {
+    boardState = currBoard;
+    for(int i = 0; i < boardState.size(); i++) {
+        for(int j = 0; j < boardState.size(); j++) {
+            if(boardState[i][j] == 0) {
+                zeroRow = i;
+                zeroColumn = j;
+                break;
+            }
+        }
+    }
+    this->fCost = fCost;
+    this->depth = depth;
+}
+
+bool operator<(const node& n1, const node& n2) {
+    return n1.fCost < n2.fCost;
+}
 
 class puzzleSolver {
     private:
         vector<vector<int>> gameBoard {
             {1, 2, 3},
-            {4, 8, 0},
-            {7, 6, 5}
+            {4, 5, 6},
+            {0, 7, 8}
         };
         int customBoard = 0;
         int algorithm = 0;
@@ -27,8 +77,10 @@ class puzzleSolver {
         void initializeBoard();
         void initializeRowHelper(const string& input, int row);
         void algorithmChoice();
-        void uniformCostSearch();
-        void showGameBoard();
+        bool generalSearch();
+        int heuristic();
+        bool isSolution(const vector<vector<int>>&);
+        void showGameBoard(const vector<vector<int>>&);
 };
 
 void puzzleSolver::promptBoardType() {
@@ -74,14 +126,117 @@ void puzzleSolver::algorithmChoice() {
     cin >> algorithm;
 }
 
-void puzzleSolver::uniformCostSearch() {
+bool puzzleSolver::generalSearch() {
+    auto start = high_resolution_clock::now();
+    priority_queue<node> nodes;
+    node firstNode(gameBoard);
+    int expandedNodes = 0;
+    int maxQueueSize = 0;
 
+    nodes.push(firstNode);
+
+    while(!nodes.empty()) {
+        node currNode = nodes.top();
+        nodes.pop();
+        
+        if(isSolution(currNode.boardState)) {
+            cout << "Goal!" << endl;
+            cout << "To solve this problem the search algorithm expanded a total of " << expandedNodes << " nodes." << endl;
+            cout << "The maximum number of nodes in the queue at any one time was " << maxQueueSize << endl;
+            cout << "The depth of the goal node was " << currNode.depth << endl;
+            return true;
+        }
+
+        // Move up
+        if(currNode.zeroRow > 0) {
+            node temp = currNode;
+            int row = temp.zeroRow;
+            int column = temp.zeroColumn;
+            temp.boardState[row][column] = temp.boardState[row-1][column];
+            temp.boardState[row-1][column] = 0;
+            temp.zeroRow = row - 1;
+            temp.depth += 1;
+            temp.fCost = 1 + heuristic();
+            nodes.push(temp);
+            expandedNodes++;
+        }
+        // Move right
+        if(currNode.zeroColumn < 2) {
+            node temp = currNode;
+            int row = temp.zeroRow;
+            int column = temp.zeroColumn;
+            temp.boardState[row][column] = temp.boardState[row][column+1];
+            temp.boardState[row][column+1] = 0;
+            temp.zeroColumn = column + 1;
+            temp.depth += 1;
+            temp.fCost = 1 + heuristic();
+            nodes.push(temp);
+            expandedNodes++;
+        }
+        // Move down
+        if(currNode.zeroRow < 2) {
+            node temp = currNode;
+            int row = temp.zeroRow;
+            int column = temp.zeroColumn;
+            temp.boardState[row][column] = temp.boardState[row+1][column];
+            temp.boardState[row+1][column] = 0;
+            temp.zeroRow = row + 1;
+            temp.depth += 1;
+            temp.fCost = 1 + heuristic();
+            nodes.push(temp);
+            expandedNodes++;
+        }
+        // Move left
+        if(currNode.zeroColumn > 0) {
+            node temp = currNode;
+            int row = temp.zeroRow;
+            int column = temp.zeroColumn;
+            temp.boardState[row][column] = temp.boardState[row][column-1];
+            temp.boardState[row][column-1] = 0;
+            temp.zeroColumn = column - 1;
+            temp.depth += 1;
+            temp.fCost = 1 + heuristic();
+            nodes.push(temp);
+            expandedNodes++;
+        }
+
+        if(nodes.size() > maxQueueSize) {
+            maxQueueSize = nodes.size();
+        }
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        if(duration.count() > 6e+7) {
+            return false;
+        }
+    }
+
+    return false;
 }
 
-void puzzleSolver::showGameBoard() {
-    for(int i = 0; i < gameBoard.size(); i++) {
-        for(int j = 0; j < gameBoard[0].size(); j++) {
-            cout << gameBoard[i][j] << " ";
+int puzzleSolver::heuristic() {
+    if(algorithm == 2) {
+        // calculate # of misplaced tiles
+    }
+    else if(algorithm == 3) {
+        // calculate manhattan distance
+    }
+    // for uniform cost search
+    return 0;
+}
+
+bool puzzleSolver::isSolution(const vector<vector<int>>& currBoard) {
+    const vector<vector<int>> solution {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 0}
+    };
+    return (currBoard == solution ? true : false);
+}
+
+void puzzleSolver::showGameBoard(const vector<vector<int>>& board) {
+    for(int i = 0; i < board.size(); i++) {
+        for(int j = 0; j < board[0].size(); j++) {
+            cout << board[i][j] << " ";
         }
         cout << endl;
     }
@@ -91,18 +246,9 @@ void puzzleSolver::driverFunction() {
     promptBoardType();
     initializeBoard();
     algorithmChoice();
+    generalSearch();
 
-    if(algorithm == 1) {
-        uniformCostSearch();
-    }
-    else if(algorithm == 2) {
-
-    }
-    else if(algorithm == 3) {
-
-    }
-
-    showGameBoard();
+    // showGameBoard();
 }
 
 
